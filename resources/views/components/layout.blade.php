0a9fa182b122365281feb3078fc0e2398700a627
George Medina <geo.f.medina@gmail.com>
2025-04-12 15:43:38 -0500

    16-17

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -96,8 +96,9 @@
     </nav>
 
     <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
             <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+            <a href="/jobs/create" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium  text-gray-700"  >Create Job</a>
         </div>
     </header>
 
--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,33 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(5);
+    $jobs = Job::with('employer')->latest()->simplePaginate(5);
 //    return view('jobs', ['jobs' => $jobs]);
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
+});
+Route::post('/jobs', function () {
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required']
+        ]);
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+    return redirect('/jobs');
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
