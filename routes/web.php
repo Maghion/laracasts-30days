5e1520c825aa8b3f58c026a8418688f09d3ab586
George Medina <geo.f.medina@gmail.com>
2025-04-27 15:13:54 -0500

    18-19 Laracast

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,13 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(5);
-//    return view('jobs', ['jobs' => $jobs]);
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-Route::post('/jobs', function () {
-        request()->validate([
-            'title' => ['required', 'min:3'],
-            'salary' => ['required']
-        ]);
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-    return redirect('/jobs');
-});
+Route::view("/", "home");
+Route::view("/contact", "contact");
 
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
+Route::resource('jobs', JobController::class);
 
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
 
-    return view('jobs.show', ['job' => $job]);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
